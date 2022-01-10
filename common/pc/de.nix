1cd488581cfee327ca44412ca8bdcd1bd945e728
Anthony DeDominic <adedomin@gmail.com>
2022-01-10 12:36:10 -0500

    weak bind noto emoji to get color emojis everywhere.

--- common/pc/de.nix
+++ common/pc/de.nix
@@ -37,6 +37,18 @@ in {
       ];
       fontconfig = {
         localConf = ''
+        <match target="pattern">
+         <test qual="any" name="family"><string>monospace</string></test>
+         <edit name="family" mode="append" binding="weak"><string>Noto Color Emoji</string></edit>
+        </match>
+        <match target="pattern">
+         <test qual="any" name="family"><string>serif</string></test>
+         <edit name="family" mode="append" binding="weak"><string>Noto Color Emoji</string></edit>
+        </match>
+        <match target="pattern">
+         <test qual="any" name="family"><string>sans-serif</string></test>
+         <edit name="family" mode="append" binding="weak"><string>Noto Color Emoji</string></edit>
+        </match>
         <match target="font">
           <edit mode="assign" name="autohint"><bool>false</bool></edit>
           <edit mode="assign" name="hinting"><bool>true</bool></edit>
