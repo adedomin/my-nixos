7e5bd098f31512376c5a74f9743a2de5812d578d
Anthony DeDominic <adedomin@gmail.com>
2023-03-09 11:39:17 -0500

    update inputs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
   };
 
