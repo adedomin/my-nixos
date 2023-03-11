53476b3d8813ebc3132ae3fe8e0521a136538dba
Anthony DeDominic <adedomin@gmail.com>
2023-03-11 16:36:56 -0500

    aaaaaaaaaaaaaaaaaaa

--- machines/wsl2/configuration.nix
+++ machines/wsl2/configuration.nix
@@ -17,6 +17,6 @@
 
   wsl2.enable = true;
 
-  required.password = "$6$tybqoFUtJoqxmVHZ$/IO40qvuHqccLIr/OGqkWFA8q0rTjeiUhO1yNyT.Moobphmn/g9oByXNbALvDwo9M1rk4R.jBPFc3RuZDZLXH/"
+  required.password = "$6$tybqoFUtJoqxmVHZ$/IO40qvuHqccLIr/OGqkWFA8q0rTjeiUhO1yNyT.Moobphmn/g9oByXNbALvDwo9M1rk4R.jBPFc3RuZDZLXH/";
   home-manager.users.adedomin.git.email = "adedomin@gmail.com";
 }
